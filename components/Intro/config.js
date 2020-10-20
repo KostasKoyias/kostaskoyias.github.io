5ce758240b33b9287326cea86079b9323dee9d9f
KostasKoyias <sdi1500071@di.uoa.gr>
2020-10-20 11:20:00 +0200

    Modify picture location

--- components/Intro/config.js
+++ components/Intro/config.js
@@ -1,5 +1,4 @@
-const picture = "http://cgi.di.uoa.gr/~sdi1500071/.picture"
-                
+const picture = "https://lh3.googleusercontent.com/pw/ACtC-3e3bpcy7FSqVhZ41ybkYq3V-Mb6Upikok0YS3kdBjAfNdx1QyUWus9NXElwTAuQluqYJMpUeVuw5GgTIPJgiTOgMLKUemUaYe9gnfhIIRhE-kOFWenoLVRQscX7qHnphQgf5baVXcIhQB6fpBO6L12O=s1664-no?authuser=0"
 
 const interests = [
     "Competitive Programming",
